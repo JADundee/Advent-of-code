5323661e0ad64ddb580c759c0b2a8cb52de2d8c4
JADundee <Jadundee@icloud.com>
2023-11-13 12:01:32 -0700

    day 1 challenge part 2

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const fs = require('fs')
 
-
+//Question 1 PART 1
+/* const fs = require('fs')
 
 function q1() {
     fs.readFile('./santa.txt', (err, data) => {
@@ -18,4 +18,29 @@ function q1() {
     })
 }
 
-q1()
+q1() */
+
+//Question 1 PART 2
+const fs = require('fs')
+
+function q2() {
+    fs.readFile('./santa.txt', (err, data) => {
+        const directions = data.toString()
+        const dArray = directions.split('')
+        let number = 0
+        let counter = 0
+        const answer = dArray.some((currentValue) => {
+            if (currentValue === '(') {
+                 number += 1
+            } else if (currentValue === ')') {
+                 number -=1
+            }
+            counter ++
+            return number < 0
+        })
+        console.log(counter)
+    })
+    
+}
+
+q2()
